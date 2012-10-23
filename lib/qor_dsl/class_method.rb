183742640be1106b8fd5f38dcd7d822b321655c1
Jinzhu <wosmvp@gmail.com>
2012-10-23 17:46:17 +0800

    Raise exception if no configuration found

--- lib/qor_dsl/class_method.rb
+++ lib/qor_dsl/class_method.rb
@@ -45,7 +45,7 @@ module Qor
       end
 
       def load_file(file)
-        return unless File.exist?(file.to_s)
+        raise("No configuration found!") unless File.exist?(file.to_s)
         node_root.config.instance_eval(File.read(file))
         node_root
       end
